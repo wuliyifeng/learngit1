d83e23861bbbb00e2a37ce1d016c5d58809227bb
lyf <gmy15389695899@sina.com>
2016-06-07 23:33:39 +0800

    some files

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,24 @@
 'use strict';
 
 function thousands_separators(num) {
+	 var Part = ''; 
+    num = num.toString();
+    if ( num.indexOf( '.' ) != -1 ) { 
+        Part = '.'+ num.split( '.' )[1]; 
+        num = parseInt(num.split( '.' )[0]);
+    } 
+    var array = num.toString().split( '' );
+    var index = -3;
+    while ( array.length + index > 0 ){ 
+        array.splice( index, 0, ',' ); 
+        // Decrement by 4 since we just added another unit to the array.
+        index -= 4; 
+    }
+    var result=array.join( '' ) + Part;
+    return result; 
   
 }
+var num='1234.1234'
+thousands_separators(num);
 
 module.exports = thousands_separators;
